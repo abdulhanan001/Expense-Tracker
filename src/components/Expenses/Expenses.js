0e7431931001f5a1f62855e3cf464233442adf9b
abdulhanan001 <abdulhanan347@gmail.com>
2022-04-03 01:57:59 +0500

    ExpenseChart::Chart Added chart ups down dymic functionality

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,6 +2,7 @@ import React from "react"; // add React only for follow convention for jsx
 import { useState } from "react";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2022");
@@ -20,6 +21,7 @@ const Expenses = (props) => {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
+      <ExpensesChart expenses={filteredExpenses} />
       <h2>Expense Tracking App</h2>
       <ExpensesList expenses={filteredExpenses} />
     </div>
